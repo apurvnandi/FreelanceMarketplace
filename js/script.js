0b4e4b1f14047d735cb75dfe8042eb7c4bc8fcda
Apurv Nandi <2024ci_apurvnandi_a@nie.ac.in>
2026-04-16 02:36:26 +0530

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,61 @@
+function registerUser(){
+
+let name=document.getElementById("regName").value;
+let email=document.getElementById("regEmail").value;
+let password=document.getElementById("regPassword").value;
+let role=document.getElementById("regRole").value;
+
+if(name===""||email===""||password===""){
+alert("Fill All Fields");
+return;
+}
+
+localStorage.setItem("userName",name);
+localStorage.setItem("userEmail",email);
+localStorage.setItem("userPassword",password);
+localStorage.setItem("userRole",role);
+
+alert("Registration Successful!");
+
+window.location.href="login.html";
+
+}
+
+
+
+function loginUser(){
+
+let email=document.getElementById("loginEmail").value;
+let password=document.getElementById("loginPassword").value;
+
+let storedEmail=localStorage.getItem("userEmail");
+let storedPassword=localStorage.getItem("userPassword");
+let role=localStorage.getItem("userRole");
+
+if(email===storedEmail&&password===storedPassword){
+
+alert("Login Successful");
+
+if(role==="freelancer"){
+window.location.href="freelancer-dashboard.html";
+}
+
+else{
+window.location.href="client-dashboard.html";
+}
+
+}
+
+else{
+alert("Invalid Credentials");
+}
+
+}
+
+
+
+function logoutUser(){
+
+window.location.href="index.html";
+
+}
